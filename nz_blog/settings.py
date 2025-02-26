0858782290acb13d5b8824cf7906fb607331f941
shinezeee <h46998600@gmail.com>
2025-02-26 18:27:46 +0900

    create Post model

--- nz_blog/settings.py
+++ nz_blog/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "nz_blog",
-    "like.apps.LikeConfig",
+    "blog",
+    "like",
 ]
 
 MIDDLEWARE = [

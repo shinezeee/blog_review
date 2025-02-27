46b36407798074e26bd5c8bfb8f5a8330fcd7cfd
shinezeee <h46998600@gmail.com>
2025-02-27 18:33:27 +0900

    templates 수정

--- nz_blog/urls.py
+++ nz_blog/urls.py
@@ -12,16 +12,13 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('post/', include('post.urls'))
+    2. Add a URL to urlpatterns:  path('templates/', include('templates.urls'))
 """
 
 from django.contrib import admin
 from django.urls import include, path
 
-import post
-from post.views import post_delete, post_detail, post_edit, post_list, post_new
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("post.urls")),  # post url 포함시키기
+    path("", include("post.urls")),  # templates url 포함시키기
 ]

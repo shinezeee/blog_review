b95627369283cc33bf41ef1c701859ffc59804e9
shinezeee <h46998600@gmail.com>
2025-02-27 16:56:04 +0900

    change app name 'blog'->'post'

--- post/views.py
+++ post/views.py
@@ -9,13 +9,13 @@ from post.models import Post
 # 게시글 목록 보기
 def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.objects.all().order_by("-created_at")
-    return render(request, "blog/post_list.html", {"posts": posts})
+    return render(request, "post/post_list.html", {"posts": posts})
 
 
 # 게시글 상세보기
 def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
     post = get_object_or_404(Post, id=post_id)
-    return render(request, "blog/post_detail.html", {"post": post})
+    return render(request, "post/post_detail.html", {"post": post})
 
 
 # 게시글 작성
@@ -25,7 +25,7 @@ def post_new(request: HttpRequest) -> HttpResponse:
         content: str = request.POST.get("content", "")
         Post.objects.create(title=title, content=content)
         return redirect("post_list")
-    return render(request, "blog/post_form.html")
+    return render(request, "post/post_form.html")
 
 
 # 게시글 수정
@@ -36,7 +36,7 @@ def post_edit(request: HttpRequest, post_id: int) -> HttpResponse:
         post.content = request.POST.get("content", post.content) or post.content  # None 방지
         post.save()
         return redirect("post_detail", post_id=post_id)
-    return render(request, "blog/post_form.html", {"post": post})
+    return render(request, "post/post_form.html", {"post": post})
 
 
 # 게시글 삭제
